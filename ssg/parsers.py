d23e0af3bf1ade4dcb71d11a2a9402bfaf0aa841
Rajgopal Shrinivasan <rshrini@ra.rockwell.com>
2020-06-06 18:46:00 -0400

    The Parser Class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -6,20 +6,20 @@ class Parser:
     extensions:List[str] = []
     def valid_extension(self,extension):
         return extension in self.extensions
-    def parse(self, path, source, dest):
+    def parse(self, path:Path , source:Path , dest:Path ):
         raise NotImplementedError
     def read(self,path):
-        with path.open() as f:
-            return f.read()
+        with path.open() as file:
+            return file.read()
     def write(self,path, dest, content,ext=".html"):
         full_path = dest / path.with_suffix(ext).name
-        with full_path.open() as f:
-            f.write(content)
+        with full_path.open() as file:
+            file.write(content)
     def copy(self,path, source, dest):
         shutil.copy2(path ,dest / path.relative_to(source))
 
 class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css",".html"]
 
-    def parse(self, path, source, dest):
+    def parse(self, path:Path , source:Path , dest:Path ):
         Parser.copy(path, source, dest)
