15617b7e9f9f32345fbe846ae8e1f78c30f68bc6
Rajgopal Shrinivasan <rshrini@ra.rockwell.com>
2020-06-06 18:55:18 -0400

    The Parser Class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -22,4 +22,4 @@ class ResourceParser(Parser):
     extensions = [".jpg", ".png", ".gif", ".css",".html"]
 
     def parse(self, path:Path , source:Path , dest:Path ):
-        Parser.copy(path, source, dest)
+        Parser.copy(self,path, source, dest)
