a3fede2d5a31aa4ab6f5920687c83a18ef998aaf
Rajgopal Shrinivasan <rshrini@ra.rockwell.com>
2020-06-06 18:58:09 -0400

    The Parser Class

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -9,11 +9,11 @@ class Parser:
     def parse(self, path:Path , source:Path , dest:Path ):
         raise NotImplementedError
     def read(self,path):
-        with path.open() as file:
+        with open(path,"r") as file:
             return file.read()
     def write(self,path, dest, content,ext=".html"):
         full_path = dest / path.with_suffix(ext).name
-        with full_path.open() as file:
+        with open(full_path, "w") as file:
             file.write(content)
     def copy(self,path, source, dest):
         shutil.copy2(path ,dest / path.relative_to(source))
