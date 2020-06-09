5d1d6bf197e419b3469bc1d4e2d0fd400339bae9
Rajgopal Shrinivasan <rshrini@ra.rockwell.com>
2020-06-08 22:32:28 -0400

    The Content Class

--- ssg/content.py
+++ ssg/content.py
@@ -6,6 +6,11 @@ class Content(Mapping):
     __delimeter = r"^(?:-|+){3}\s*$"
     __regex = re.compile(__delimeter, re.MULTILINE)
 
+    @classmethod 
+    def load(cls ,string):
+        _, fm, content = cls.__regex.split(string,2)
+        metadata = load(fm,Loader=FullLoader)
+        return cls(metadata,content)
 
     def __init__(self,metadata ,content):
         self.data = metadata
@@ -39,8 +44,3 @@ class Content(Mapping):
                 data[key] = value
         return str(data)
 
-    @classmethod 
-    def load(cls ,string):
-        (_, fm, content) = cls.__regex.split(string,2)
-        load(fm,Loader=FullLoader)
-        return cls(metadata,content)
